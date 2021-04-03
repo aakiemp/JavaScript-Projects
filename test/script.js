0cd65fa44e37545d3602c9419a052327d8a2cf4e
asp16f <asp16f@my.fsu.edu>
2021-04-03 00:48:59 -0400

    4/3

--- test/script.js
+++
@@ -1,8 +0,0 @@
-let myFish = ['angel', 'clown', 'mandarin', 'sturgeon']
-let removed = myFish.splice(2, 1, 'drum', 'guitar')
-
-// myFish is ["angel", "clown", "drum", "guitar", "mandarin", "sturgeon"]
-// removed is [], no elements removed
-
-console.log(myFish)
-console.log(removed)
